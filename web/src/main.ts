771632abf0a433a1e9d7d273e5ae668adf58de90
Eren <eren@awydd.com>
2026-08-27 09:01:54 +0800

    自定义主题

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import './styles/index.scss'
 
 const app = createApp(App)
 
